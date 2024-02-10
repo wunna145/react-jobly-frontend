54dceb737e8f6ce78d75725a77d379a12c9577b6
Zaw Wunna <zawwunna@Zaws-Air.lan>
2024-02-10 02:15:13 -0800

    files added

---
+++ src/profiles/ProfileForm.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Profile from "./ProfileForm";
+import { UserProvider } from "../testUtils";
+
+it("matches snapshot", function () {
+  const { asFragment } = render(
+      <UserProvider>
+        <Profile />
+      </UserProvider>,
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
---
+++ src/routes-nav/Routes.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Homepage from "../homepage/Homepage";
+import CompanyList from "../companies/CompanyList";
+import JobList from "../jobs/JobList";
+import CompanyDetail from "../companies/CompanyDetail";
+import LoginForm from "../auth/LoginForm";
+import ProfileForm from "../profiles/ProfileForm";
+import SignupForm from "../auth/SignupForm";
+import PrivateRoute from "./PrivateRoute";
+
+function Routes({ login, signup }) {
+  console.debug(
+      "Routes",
+      `login=${typeof login}`,
+      `register=${typeof register}`,
+  );
+
+  return (
+      <div className="pt-5">
+        <Switch>
+
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+
+          <Route exact path="/login">
+            <LoginForm login={login} />
+          </Route>
+
+          <Route exact path="/signup">
+            <SignupForm signup={signup} />
+          </Route>
+
+          <PrivateRoute exact path="/companies">
+            <CompanyList />
+          </PrivateRoute>
+
+          <PrivateRoute exact path="/jobs">
+            <JobList />
+          </PrivateRoute>
+
+          <PrivateRoute exact path="/companies/:handle">
+            <CompanyDetail />
+          </PrivateRoute>
+
+          <PrivateRoute path="/profile">
+            <ProfileForm />
+          </PrivateRoute>
+
+          <Redirect to="/" />
+        </Switch>
+      </div>
+  );
+}
+
+export default Routes;
